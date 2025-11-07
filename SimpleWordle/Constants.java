a05bf3f6e594d2fafa4a9c466105aa1e95d6cefe
Vanish-Coder <vanshuz@icloud.com>
2025-11-06 21:02:45 -0800

    Update Spacing

--- SimpleWordle/Constants.java
+++ SimpleWordle/Constants.java
@@ -14,7 +14,7 @@ public class Constants
 		"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
 		  "A", "S", "D", "F", "G", "H", "J", "K", "L", 
 		"ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE",
-							"RESET WORD"
+							"RESET"
 	};
 	
 	public static final int [] KEYS = 
@@ -33,3 +33,4 @@ public class Constants
 	};
 
 }
+
